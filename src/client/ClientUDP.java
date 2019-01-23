920261ef9af39808b6a900da30421c066ae9597b
Mickael <mickael.spilmont.fr@gmail.com>
2019-01-23 13:20:53 +0100

    Ajout de commentaires explicatifs

--- src/client/ClientUDP.java
+++ src/client/ClientUDP.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.util.Scanner;
 
 public class ClientUDP {
@@ -16,17 +17,26 @@ public class ClientUDP {
 
 	void envoie() throws IOException {
 		DatagramPacket dgPacket = new DatagramPacket(new byte[0], 0);
+		
+//		On entre en entête les info de la machine avec laquelle on désire communiquer
+//		Pour ça deux méthodes :
+		/*
 		dgPacket.setAddress(InetAddress.getByName("hevea13"));
 		dgPacket.setPort(9876);
+		*/
+		dgPacket.setSocketAddress(new InetSocketAddress("hevea13", 9876));
 				
-//		On envoie le message
+//		On envoie le paquet (sans message)
 		dgSocket.send(dgPacket);
 
+//		On prépare un buffer pour la réception de la réponse
 		byte[] bufReception = new byte[50];
 		
+//		On indique le buffer dans le datagrame et on se met en attente de reception
 		dgPacket.setData(bufReception);
 		dgSocket.receive(dgPacket);
 		
+//		On reconvertie les infos reçues en string et on les affiches
 		String str = new String(dgPacket.getData(), dgPacket.getOffset(), dgPacket.getLength());
 		System.out.println(str);
 	}
